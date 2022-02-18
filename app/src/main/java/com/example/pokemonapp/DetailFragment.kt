35999d7e92f8cd89e6df4a3e11be0548a0206de8
unknown <jeferson.narvaez.dev@gmail.com>
2022-02-18 11:38:49 -0500

    Add spinner to load of image

--- app/src/main/java/com/example/pokemonapp/DetailFragment.kt
+++ app/src/main/java/com/example/pokemonapp/DetailFragment.kt
@@ -1,13 +1,20 @@
 package com.example.pokemonapp
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,11 +27,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DetailFragment : Fragment() {
-    private lateinit var imageView:ImageView
-    private lateinit var hpText:TextView
-    private lateinit var attackText:TextView
-    private lateinit var defenseText:TextView
-    private lateinit var speedText:TextView
+    private lateinit var imageView: ImageView
+    private lateinit var hpText: TextView
+    private lateinit var attackText: TextView
+    private lateinit var defenseText: TextView
+    private lateinit var speedText: TextView
+    private lateinit var loading: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,19 +41,45 @@ class DetailFragment : Fragment() {
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_detail, container, false)
 
-        imageView=rootView.findViewById(R.id.iv_detail)
-        hpText=rootView.findViewById(R.id.tv_hp)
-        attackText=rootView.findViewById(R.id.tv_attack)
-        defenseText=rootView.findViewById(R.id.tv_defense)
-        speedText=rootView.findViewById(R.id.tv_speed)
+        imageView = rootView.findViewById(R.id.iv_detail)
+        hpText = rootView.findViewById(R.id.tv_hp)
+        attackText = rootView.findViewById(R.id.tv_attack)
+        defenseText = rootView.findViewById(R.id.tv_defense)
+        speedText = rootView.findViewById(R.id.tv_speed)
+        loading = rootView.findViewById(R.id.pb_loading)
+
         return rootView
     }
 
-    fun setPokemonData(pokemon: Pokemon){
-        Glide.with(this).load(pokemon.url).into(imageView);
-        hpText.text = getString(R.string.hp_format,pokemon.hp)
-        attackText.text=getString(R.string.attack_format,pokemon.attack)
-        defenseText.text=getString(R.string.defense_format,pokemon.defense)
-        speedText.text=getString(R.string.speed_format,pokemon.speed)
+    fun setPokemonData(pokemon: Pokemon) {
+        loading.visibility = View.VISIBLE
+        Glide.with(this).load(pokemon.url).listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                loading.visibility = View.GONE
+                imageView.setImageResource(R.drawable.ic_baseline_image_not_supported_24)
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                loading.visibility = View.GONE
+                return false
+            }
+
+        }).into(imageView)
+        hpText.text = getString(R.string.hp_format, pokemon.hp)
+        attackText.text = getString(R.string.attack_format, pokemon.attack)
+        defenseText.text = getString(R.string.defense_format, pokemon.defense)
+        speedText.text = getString(R.string.speed_format, pokemon.speed)
     }
 }
